bfa9a801a10d88ddbffb7b5ebed047f2edbf3eec
wygd <yuvinygd@gmail.com>
2022-04-01 23:25:24 -0600

    se agrego credenciales firebase

--- FrontendUI/src/environments/environment.prod.ts
+++ FrontendUI/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  base_url: "http://34.133.92.54:5000/"
+  base_url: "http://34.133.92.54:5000/",
+  firebaseConfig : {
+    apiKey: "AIzaSyCVxXCkDq49vW4ypQkuERQ7d8x9393rGVU",
+    authDomain: "sa2022-2cf26.firebaseapp.com",
+    projectId: "sa2022-2cf26",
+    storageBucket: "sa2022-2cf26.appspot.com",
+    messagingSenderId: "940364599647",
+    appId: "1:940364599647:web:397f61e56e62be84c126ea",
+    measurementId: "G-2S239T558B"
+  }
 };
